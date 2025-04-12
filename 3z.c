6722a59b50217da74b87b8919fbc849fda139a1b
scrimshandy <synsmtv22@gmail.com>
2025-04-12 12:42:53 +0300

    Update 3z.c

--- 3z.c
+++ 3z.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-  int colorful_balls[15];
-  int size = sizeof(colorful_balls) / sizeof(colorful_balls[0]);
-  printf("В коробке colorful_balls помещается %d шариков.\n", size);
+   int colorful_balls[15] = {15,14,13,12,11,10,9,8,7,6,5,4,3,2,1}; 
+  int colorful_count = sizeof(colorful_balls) / sizeof(colorful_balls[0]);
+  printf("Количество разноцветных шариков: %d\n", colorful_count);
   return 0;
 }
