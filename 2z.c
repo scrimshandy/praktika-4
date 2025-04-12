05f20d7667bcc3d731ee445f3a1768471706500c
scrimshandy <synsmtv22@gmail.com>
2025-04-12 12:42:26 +0300

    Update 2z.c

--- 2z.c
+++ 2z.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-  int blue_balls[5];
-  int size = sizeof(blue_balls) / sizeof(blue_balls[0]);
-  printf("В коробке blue_balls помещается %d шариков.\n", size);
+   int blue_balls[5] = {2,2,2,2,2}; 
+    int blue_count = sizeof(blue_balls) / sizeof(blue_balls[0]);
+    printf("Количество синих шариков: %d\n", blue_count);
   return 0;
 }
