d943b7bcfb7ba9a93cdc2e24b15d7bd304d8915e
scrimshandy <synsmtv22@gmail.com>
2025-04-12 12:41:43 +0300

    Update 1z.c

--- 1z.c
+++ 1z.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-  int red_balls[10];
-  int size = sizeof(red_balls) / sizeof(red_balls[0]);
-  printf("В коробке red_balls помещается %d шариков.\n", size);
+int red_balls[10] = {1,1,1,1,1,1,1,1,1,1}; 
+    int red_count = sizeof(red_balls) / sizeof(red_balls[0]);
+    printf("Количество красных шариков: %d\n", red_count);
   return 0;
 }
